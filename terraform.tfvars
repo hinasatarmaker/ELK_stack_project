fad5baef7319ee41d39e85b7dd24fdb587dcee05
Hina Satarmaker <hina.satarmaker@cloudreach.com>
2022-08-25 13:01:37 +0530

    variables updated

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+kibana_ami_id              = "ami-0407b127d8017cf3a"
+elasticsearch_ami_id       = "ami-0d61dd26146c40de8"
+logstash_ami_id            = "ami-06d85d5be3041bf5d"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "kibana_ami_id" {
+    description = "This is my ami id for Kibana"
+    type        = string
+}
+
+variable "elasticsearch_ami_id" {
+    description = "This is my ami id for ElasticSearch"
+    type        = string
+}
+
+variable "logstash_ami_id" {
+    description = "This is my ami id for Logstash"
+    type        = string
+}
